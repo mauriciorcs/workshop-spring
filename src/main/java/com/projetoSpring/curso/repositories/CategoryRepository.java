d765d2611e75e3412b26e802ff104ae119f44cc9
Maurício Rodrigues <mauri.silva200021@gmail.com>
2023-11-28 14:10:27 -0300

    Category

--- src/main/java/com/projetoSpring/curso/repositories/CategoryRepository.java
+++ src/main/java/com/projetoSpring/curso/repositories/CategoryRepository.java
@@ -1,14 +1,15 @@
 package com.projetoSpring.curso.repositories;
 
+import com.projetoSpring.curso.entities.Category;
+import com.projetoSpring.curso.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import com.projetoSpring.curso.entities.User;
 
 //spring dataJpa ja tem uma implementação para a inteface
 //se for definido JpaRepository com minha entidade e o tipo do seu ID(implementação padrão)
 
 @Repository //-> opicional
-public interface UserRepository extends JpaRepository<User, Long>{
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+	
 
 }
