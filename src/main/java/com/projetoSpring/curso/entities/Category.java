a50e215a1785fba63863a2fea8a6374d486cdc10
Maurício Rodrigues <mauri.silva200021@gmail.com>
2024-10-27 00:58:21 -0300

    de-volta-ao-spring

--- src/main/java/com/projetoSpring/curso/entities/Category.java
+++ src/main/java/com/projetoSpring/curso/entities/Category.java
@@ -1,5 +1,6 @@
 package com.projetoSpring.curso.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -17,7 +18,8 @@ public class Category implements Serializable {
     private Long id;
     private String nome;
 
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category (Long id, String nome){
